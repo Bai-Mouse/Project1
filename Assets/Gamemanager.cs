beb7179e7b0064a1905a8b6df1bbc31a2477eb34
Bai-Mouse <yixuanshi059@gmail.com>
2024-09-04 16:45:01 -0400

    9/4

--- Assets/Gamemanager.cs
+++ Assets/Gamemanager.cs
@@ -7,6 +7,10 @@ public class Gamemanager : MonoBehaviour
     //public Time passed variable;
     public float myTimer = 0;
     public float myFixedTimer=0;
+
+    public GameObject Enemy;
+    public float spawnInterval = .5f;
+    public float spawnTimer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,14 @@ public class Gamemanager : MonoBehaviour
     void Update()
     {
         myTimer += Time.deltaTime;
+
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnInterval)
+        {
+            spawnTimer = 0;
+            Debug.Log("Spawn");
+            Instantiate(Enemy);
+        }
         
     }
     private void FixedUpdate()
