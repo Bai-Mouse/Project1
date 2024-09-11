4a2496c2d8c6941cf042db35d8517fbdbf45051f
Bai-Mouse <yixuanshi059@gmail.com>
2024-09-11 16:34:52 -0400

    9/11

--- Assets/Gamemanager.cs
+++ Assets/Gamemanager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Gamemanager : MonoBehaviour
@@ -7,14 +8,16 @@ public class Gamemanager : MonoBehaviour
     //public Time passed variable;
     public float myTimer = 0;
     public float myFixedTimer=0;
-
+    public GameObject Player;
     public GameObject Enemy;
     public float spawnInterval = .5f;
     public float spawnTimer = 0f;
+    public float myscore;
+    public TextMeshProUGUI TextMeshPro;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -27,9 +30,13 @@ public class Gamemanager : MonoBehaviour
         {
             spawnTimer = 0;
             Debug.Log("Spawn");
-            Instantiate(Enemy);
+            GameObject newEnemy = Instantiate(Enemy);
+            newEnemy.transform.position += new Vector3(Random.Range(-10, 10),0,0);
+
+
         }
-        
+        myscore = Player.GetComponent<Player>().score;
+        TextMeshPro.text = "Score: "+myscore.ToString();
     }
     private void FixedUpdate()
     {
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float speed = 1f;
+    public float score;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +28,13 @@ public class Player : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
 
         return new Vector3(x, y, 0);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Collectible")
+        {
+            Destroy(collision.gameObject);
+            score++;
+        }
     }
 }
